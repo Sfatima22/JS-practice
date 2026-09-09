9e9d4a839f4094999ac422e44475278ace3b2978
Sfatima22 <shaguftaf28@gmail.com>
2026-09-09 17:18:17 +0000

    Completed Array:push,pop,shift,unshift

--- 26.js
+++ 26.js
@@ -1,5 +1,8 @@
 // arrays
 let fruits =["apple", "mango", "melon"];
-console.log(fruits)
-
-console.log(fruits[3])
+// console.log(fruits)
+// let fruit =["apple", "mango", "melon"];
+// console.log(fruits[3]);
+// fruits[3]="grapes";
+// console.log(fruits);
+console.log(typeOf (fruits));
---
+++ 27.js
@@ -0,0 +1,14 @@
+// array push pop
+let fruits = ["apple", "mango", "grapes"];
+console.log(fruits)
+// fruits.push("guava");
+// (fruits.pop());
+// let poppedFruit=fruits.pop();
+// console.log(fruits);
+// console.log("popped fruits is",poppedFruit);
+
+// unshift
+// fruits.unshift("banana","melon");
+fruits.shift();
+console.log(fruits);
+
