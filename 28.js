c8eb1402ecf13066c447fdb06d492a905e10700f
Sfatima22 <shaguftaf28@gmail.com>
2026-09-10 11:22:00 +0000

    Primitive vs Reference data types

---
+++ 28.js
@@ -0,0 +1,18 @@
+// primitive vs reference data types
+// let num1=9;
+// let num2 =num1;
+// console.log("value of num1 is", num1);
+// console.log("value of num2 is", num2);
+// num1++;
+// console.log("after incrementing num1");
+// console.log("value of num1 is", num1);
+// console.log("value of num2 is ", num2);
+
+let array1=["item1","item2"]
+let array2=array1;
+console.log("array1", array1);
+console.log("array2", array2);
+array1.push("item3");
+console.log("after push");
+console.log("array1", array1);
+console.log("array2", array2);
