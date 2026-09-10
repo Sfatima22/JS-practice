58270d468a576416ee393c32724fc6d761e5410a
Sfatima22 <shaguftaf28@gmail.com>
2026-09-10 12:57:41 +0000

    for of loop in arrays

---
+++ 33.js
@@ -0,0 +1,10 @@
+// for of loop in arrays
+const fruits=["apple", "kiwi", "starfruit", "lemon"];
+for(let fruit of fruits){
+    console.log(fruits);
+}
+// let i=0;
+// while (i<fruits.length){
+//     console.log(fruits[i].toUpperCase());
+//     i++;
+// }
