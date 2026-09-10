79a813ec454010acf7cbab4156b5cd2ef7438604
Sfatima22 <shaguftaf28@gmail.com>
2026-09-10 13:16:16 +0000

    for in loop in arrays

---
+++ 34.js
@@ -0,0 +1,7 @@
+// for in loop in arrays
+const fruits=["apple", "kiwi", "starfruit", "lemon"];
+const fruits2=[];
+for(let index in fruits){
+    fruits2.push(fruits [index]. toLocaleUpperCase());
+}
+console.log(fruits2);
